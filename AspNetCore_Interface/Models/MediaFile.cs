0fd29d6e523e92a924521169bddde0e03ddca20c
MihaelaPotlog <mihaela.potlog@info.uaic.ro>
2020-05-20 14:24:25 +0300

    inceput proiect-3

---
+++ AspNetCore_Interface/Models/MediaFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore_Interface.Models
+{
+    public class MediaFile
+    {
+      
+        public int Id { get; set; }
+        
+        public string Path { get; set; }
+    
+        public string Name { get; set; }
+        
+        public bool Type { get; set; }
+        
+        public DateTime Date { get; set; }
+
+        public IList<Property> Properties { get; set; }
+    }
+}
---
+++ AspNetCore_Interface/Pages/Photos/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore_Interface.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServiceReferenceMyPhotos;
+namespace AspNetCore_Interface.Pages.Photos
+{
+    public class IndexModel : PageModel
+    {
+        MyPhotosServiceClient myPhotosService = new MyPhotosServiceClient();
+        public List<MediaFile> MediaFiles { get; set; }
+        private readonly IMapper _mapper;
+
+        public IndexModel(IMapper mapper)
+        {
+            _mapper = mapper;
+            MediaFiles = new List<MediaFile>();
+        }
+        public async Task  OnGetAsync()
+        {
+            var mediaFiles = await myPhotosService.GetAllAsync();
+            Console.WriteLine(MediaFiles.Count);
+            MediaFiles = _mapper.Map<List<MediaFile>>(mediaFiles);
+        }
+    }
+}
